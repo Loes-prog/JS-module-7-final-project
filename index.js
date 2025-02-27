5978ba5faf41683d3174c3814709a256c36d7144
Loes <loesvanloon@msn.com>
2025-02-27 10:34:09 +0100

    Functies toegevoegds nav opdracht: finished part 1

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const getAuthors = (recipeList) => {
   return authors;
 };
 
+console.log('---------------------------------------------------------------------------------------------');
 console.log(getAuthors(cakeRecipes));
 
 // function that logs the name of each recipe, using object destructuring. If there are no recipes found, it logs that there are no recipes found. 
@@ -29,6 +30,7 @@ const getRecipeNames = (recipeList) => {
   }
 }
 
+console.log('---------------------------------------------------------------------------------------------');
 getRecipeNames(cakeRecipes);
 
 // function that return all recipes of a given author, using the filter method.
@@ -39,7 +41,8 @@ const returnRecipeNamesByAuthor = (recipeList, author) => {
   return nameList;
 };
 
-// console.log(returnRecipeNamesByAuthor(cakeRecipes, "Mary cadogan"));
+console.log('---------------------------------------------------------------------------------------------');
+console.log(returnRecipeNamesByAuthor(cakeRecipes, "Mary cadogan"));
 
 
 // function that returns a list of recipes that contain a given ingredient. The function takes a list of recipes as input and an ingredient as a string.
@@ -58,8 +61,7 @@ const returnRecipeNamesByIngredient = (recipeList, ingredient) => {
   }
 };
 
-console.log('next exercise');
-
+console.log('---------------------------------------------------------------------------------------------');
 console.log(returnRecipeNamesByIngredient(cakeRecipes, "140g caster sugar"));
 
 // function that takes a list of recipes and a name as input. Returns a single recipe that matches the given name.
@@ -74,54 +76,67 @@ const getRecipeByName = (recipeList, name) => {
   };
 };
 
+console.log('---------------------------------------------------------------------------------------------');
 console.log(getRecipeByName(cakeRecipes, "pud"));
 
-// function that returns all ingredients of a given recipe list as a single array.
+// function that returns all ingredients of a given recipe list as a single array. Using 
 
 const getAllIngredients = (recipeList) => {
-  const allIngredients = recipeList.Ingredients.reduce( => {
+  const allIngredients = recipeList.reduce((accumulator, recipe) => {
+    return accumulator.concat(recipe.Ingredients);
+  }, []);
+  return allIngredients;
+};
+
+// shorter list for input function getAllIngredients
+
+const shorterRecipeList = returnRecipeNamesByAuthor(cakeRecipes, "Mary cadogan");
+
+console.log('---------------------------------------------------------------------------------------------');
+
+console.log(getAllIngredients(shorterRecipeList));
 
 
-  // Part 2
+// Part 2
 
-  const displayMenu = () => {
-    console.log("\nRecipe Management System Menu:");
-    console.log("1. Show All Authors");
-    console.log("2. Show Recipe names by Author");
-    console.log("3. Show Recipe names by Ingredient");
-    console.log("4. Get Recipe by Name");
-    console.log("5. Get All Ingredients of Saved Recipes");
-    console.log("0. Exit");
-    const choice = prompt("Enter a number (1-5) or 0 to exit: ");
-    return parseInt(choice);
+const displayMenu = () => {
+  console.log("\nRecipe Management System Menu:");
+  console.log("1. Show All Authors");
+  console.log("2. Show Recipe names by Author");
+  console.log("3. Show Recipe names by Ingredient");
+  console.log("4. Get Recipe by Name");
+  console.log("5. Get All Ingredients of Saved Recipes");
+  console.log("0. Exit");
+  const choice = prompt("Enter a number (1-5) or 0 to exit: ");
+  return parseInt(choice);
+}
+
+
+let choice;
+
+do {
+  choice = displayMenu();
+
+  switch (choice) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+    case 4:
+
+      break;
+    case 5:
+
+      break;
+    case 0:
+      console.log("Exiting...");
+      break;
+    default:
+      console.log("Invalid input. Please enter a number between 0 and 5.");
   }
-
-
-  let choice;
-
-  do {
-    choice = displayMenu();
-
-    switch (choice) {
-      case 1:
-
-        break;
-      case 2:
-
-        break;
-      case 3:
-
-        break;
-      case 4:
-
-        break;
-      case 5:
-
-        break;
-      case 0:
-        console.log("Exiting...");
-        break;
-      default:
-        console.log("Invalid input. Please enter a number between 0 and 5.");
-    }
-  } while (choice !== 0);
+} while (choice !== 0);
